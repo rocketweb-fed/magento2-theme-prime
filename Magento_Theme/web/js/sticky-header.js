d4a95aadaa07cfa143f83968a7953c9554461ef7
Jakub Kulig <jakub.kulig@rocketweb.com>
2019-02-18 10:28:49 +0100

    Refactor sticky header functionality

---
+++ Magento_Theme/web/js/sticky-header.js
@@ -0,0 +1,57 @@
+define([
+    'jquery',
+    'domReady!'
+], function ($) {
+    'use strict';
+
+    $.widget('rw.stickyHeader', {
+        options: {
+            enableOnMobile: true,
+            stickyClass: 'sticky-header',
+            wrapper: '.page-wrapper'
+        },
+
+        _create: function () {
+            if (!this.options.disabled) {
+                this._initializeSticky(options);
+            }
+        },
+
+        _initializeSticky: function(options) {
+            const self = this;
+            const header = this.element;
+            const wrapper = $(this.options.wrapper);
+            
+            let stickyDummyHeight = header.height();
+            let scrollPosition;
+
+            if (window.innerWidth > (this.options.enableOnMobile ? 0 : 768)) {
+                $(window).scroll(function () {
+                    scrollPosition = $(window).scrollTop();
+
+                    if (scrollPosition > stickyDummyHeight) {
+                        self._enableSticky(wrapper, stickyDummyHeight);
+                    } else {
+                        self._disableSticky(wrapper);
+                    }
+                });
+            }
+        },
+
+        _enableSticky: function(el, height) {
+            if (!el.hasClass(this.options.stickyClass)) {
+                el.addClass(this.options.stickyClass);
+                $('body').css({ "padding-top": height + 'px' });
+            }
+        },
+
+        _disableSticky: function(el) {
+            if (el.hasClass(this.options.stickyClass)) {
+                el.removeClass(this.options.stickyClass);
+                $('body').css({ "padding-top": "0" });
+            }
+        }
+    });
+
+    return $.rw.stickyHeader;    
+});
