d0ce19e38fc14218da61a86b094a1aecc7b44c1a
Jakub Kulig <jakub.kulig@rocketweb.com>
2020-04-20 18:37:58 +0200

    Add lazysizes plugin for loading background images and scriptslazily

--- Magento_Theme/requirejs-config.js
+++ Magento_Theme/requirejs-config.js
@@ -1,11 +1,12 @@
 var config = {
     map: {
         '*': {
-            lazysizes:  'Magento_Theme/js/lazysizes/lazysizes.min',
-            lsBlurUp:   'Magento_Theme/js/lazysizes/plugins/ls.blur-up',
-            lsRespimg:  'Magento_Theme/js/lazysizes/plugins/ls.respimg',
-            lsObjectFit: 'Magento_Theme/js/lazysizes/plugins/ls.object-fit',
-            scrollTo:   'Magento_Theme/js/scroll-to'
+            lazysizes:      'Magento_Theme/js/lazysizes/lazysizes.min',
+            lsBlurUp:       'Magento_Theme/js/lazysizes/plugins/ls.blur-up',
+            lsRespimg:      'Magento_Theme/js/lazysizes/plugins/ls.respimg',
+            lsObjectFit:    'Magento_Theme/js/lazysizes/plugins/ls.object-fit',
+            lsUnveilHooks:  'Magento_Theme/js/lazysizes/plugins/ls.unveilhooks',
+            scrollTo:       'Magento_Theme/js/scroll-to'
         }
     }
 };
