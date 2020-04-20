d360dbc4185ded91ff6939675bb90d9651dda36c
Jakub Kulig <jakub.kulig@rocketweb.com>
2020-04-20 13:12:50 +0200

    Include polyfills for lazysizes

--- Magento_Theme/requirejs-config.js
+++ Magento_Theme/requirejs-config.js
@@ -3,6 +3,8 @@ var config = {
         '*': {
             lazysizes:  'Magento_Theme/js/lazysizes/lazysizes.min',
             lsBlurUp:   'Magento_Theme/js/lazysizes/plugins/ls.blur-up',
+            lsRespimg:  'Magento_Theme/js/lazysizes/plugins/ls.respimg',
+            lsObjectFit: 'Magento_Theme/js/lazysizes/plugins/ls.object-fit',
             scrollTo:   'Magento_Theme/js/scroll-to'
         }
     }
