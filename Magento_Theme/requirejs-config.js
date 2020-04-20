443446b056553bc42b0b7ecdbcf45d471b377b31
Jakub Kulig <jakub.kulig@rocketweb.com>
2020-04-20 12:09:36 +0200

    Add responsive images to banner carousel, include blur-up plugin for lazysizes

--- Magento_Theme/requirejs-config.js
+++ Magento_Theme/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     map: {
         '*': {
-            lazysizes:  'Magento_Theme/js/lazysizes.min',
+            lazysizes:  'Magento_Theme/js/lazysizes/lazysizes.min',
+            lsBlurUp:   'Magento_Theme/js/lazysizes/plugins/ls.blur-up',
             scrollTo:   'Magento_Theme/js/scroll-to'
         }
     }
